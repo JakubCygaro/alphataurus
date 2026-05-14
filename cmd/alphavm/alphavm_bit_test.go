13225a5820f669a37bcdb833907ee69acbebb25e
papieros <robertrugala@gmail.com>
2026-05-14 23:56:19 +0200

    AND OR tests

--- cmd/alphavm/alphavm_bit_test.go
+++ cmd/alphavm/alphavm_bit_test.go
@@ -160,3 +160,125 @@ func TestNot1(t *testing.T) {
 		t.Error(err)
 	}
 }
+func TestAnd1(t *testing.T) {
+	const stackSize int = 1 + 2 + 4 + 8
+	stack := makeTestingStack(stackSize)
+	initV := rand.Int63()
+	sV := rand.Int63()
+	stack.push(uint8(initV) & uint8(sV))
+	stack.push(uint16(initV) & uint16(sV))
+	stack.push(uint32(initV) & uint32(sV))
+	stack.push(uint64(initV) & uint64(sV))
+	rA := randomGpRegisterWord()
+	rB := nextRandomGpRegister(rA)
+	rC := nextRandomGpRegister(rB)
+	asm := fmt.Sprintf(`
+	section '.code'
+	@entry
+		mov %s, %v
+		mov %s, %v
+		;; AND SZ_8
+		mov %s, %s
+		and %s, %s
+		push %s
+		;; AND SZ_16
+		mov %s, %s
+		and %s, %s
+		push %s
+		;; AND SZ_32
+		mov %s, %s
+		and %s, %s
+		push %s
+		;; AND SZ_64
+		mov %s, %s
+		and %s, %s
+		push %s
+		exit 0
+	`,
+		regStr(rA, vm.SZ_64), initV,
+		regStr(rB, vm.SZ_64), sV,
+		regStr(rC, vm.SZ_8), regStr(rA, vm.SZ_8),
+		regStr(rC, vm.SZ_8), regStr(rB, vm.SZ_8),
+		regStr(rC, vm.SZ_8),
+		regStr(rC, vm.SZ_16), regStr(rA, vm.SZ_16),
+		regStr(rC, vm.SZ_16), regStr(rB, vm.SZ_16),
+		regStr(rC, vm.SZ_16),
+		regStr(rC, vm.SZ_32), regStr(rA, vm.SZ_32),
+		regStr(rC, vm.SZ_32), regStr(rB, vm.SZ_32),
+		regStr(rC, vm.SZ_32),
+		regStr(rC, vm.SZ_64), regStr(rA, vm.SZ_64),
+		regStr(rC, vm.SZ_64), regStr(rB, vm.SZ_64),
+		regStr(rC, vm.SZ_64),
+	)
+	if elf, err := assembleAndLink(asm); err != nil {
+		t.Error(compilationOfErr(asm))
+		t.Error(err)
+	} else if mach, err := executeStackSize(elf, uint64(stackSize)); err != nil {
+		t.Error(compilationOfErr(asm))
+		t.Error(err)
+	} else if err := expectStack(&mach, vm.VmStack(stack)); err != nil {
+		t.Error(compilationOfErr(asm))
+		t.Error(err)
+	}
+}
+func TestOr1(t *testing.T) {
+	const stackSize int = 1 + 2 + 4 + 8
+	stack := makeTestingStack(stackSize)
+	initV := rand.Int63()
+	sV := rand.Int63()
+	stack.push(uint8(initV) | uint8(sV))
+	stack.push(uint16(initV) | uint16(sV))
+	stack.push(uint32(initV) | uint32(sV))
+	stack.push(uint64(initV) | uint64(sV))
+	rA := randomGpRegisterWord()
+	rB := nextRandomGpRegister(rA)
+	rC := nextRandomGpRegister(rB)
+	asm := fmt.Sprintf(`
+	section '.code'
+	@entry
+		mov %s, %v
+		mov %s, %v
+		;; OR SZ_8
+		mov %s, %s
+		or %s, %s
+		push %s
+		;; OR SZ_16
+		mov %s, %s
+		or %s, %s
+		push %s
+		;; OR SZ_32
+		mov %s, %s
+		or %s, %s
+		push %s
+		;; OR SZ_64
+		mov %s, %s
+		or %s, %s
+		push %s
+		exit 0
+	`,
+		regStr(rA, vm.SZ_64), initV,
+		regStr(rB, vm.SZ_64), sV,
+		regStr(rC, vm.SZ_8), regStr(rA, vm.SZ_8),
+		regStr(rC, vm.SZ_8), regStr(rB, vm.SZ_8),
+		regStr(rC, vm.SZ_8),
+		regStr(rC, vm.SZ_16), regStr(rA, vm.SZ_16),
+		regStr(rC, vm.SZ_16), regStr(rB, vm.SZ_16),
+		regStr(rC, vm.SZ_16),
+		regStr(rC, vm.SZ_32), regStr(rA, vm.SZ_32),
+		regStr(rC, vm.SZ_32), regStr(rB, vm.SZ_32),
+		regStr(rC, vm.SZ_32),
+		regStr(rC, vm.SZ_64), regStr(rA, vm.SZ_64),
+		regStr(rC, vm.SZ_64), regStr(rB, vm.SZ_64),
+		regStr(rC, vm.SZ_64),
+	)
+	if elf, err := assembleAndLink(asm); err != nil {
+		t.Error(compilationOfErr(asm))
+		t.Error(err)
+	} else if mach, err := executeStackSize(elf, uint64(stackSize)); err != nil {
+		t.Error(compilationOfErr(asm))
+		t.Error(err)
+	} else if err := expectStack(&mach, vm.VmStack(stack)); err != nil {
+		t.Error(compilationOfErr(asm))
+		t.Error(err)
+	}
+}
