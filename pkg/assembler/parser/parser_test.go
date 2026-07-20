e301c98adf17dece330c9c7daaa02510f4a37ec8
papieros <robertrugala@gmail.com>
2026-07-20 02:22:40 +0200

    all parser tests currently defined pass

--- pkg/assembler/parser/parser_test.go
+++ pkg/assembler/parser/parser_test.go
@@ -39,14 +39,12 @@ var instWithError = []instErrP{
 		".*Disallowed destination register `ip`", 5),
 	asInstErrP("add r0 @ 100",
 		".*Instruction missing a comma, got `@` instead", 8),
-	// asInstErrP("add r0, noncomptime",
-	// 	".*Second operand to instruction has to be a valid register "+
-	// 	"or a compile time expression, got `noncomptime`", 9),
-	// asInstErrP("add r0, ip",
-	// 	".*Disallowed source register `ip`", 9),
-	// asInstErrP("add r0, noncomptime",
-	// 	"Second operand to instruction has to be a valid register "+
-	// 		"or a compile time expression got `noncomptime`", 9),
+	asInstErrP("add r0, noncomptime",
+		".*Disallowed source `noncomptime`", 9),
+	asInstErrP("add r0, ip",
+		".*Disallowed source register `ip`", 9),
+	asInstErrP("add noncomptime, r0",
+		".*Disallowed destination `noncomptime`.", 5),
 
 }
 
